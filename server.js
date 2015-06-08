9ee03eb1993e1515e4d926e5b4f43027395150a5
Manoj Mandam <manoj.op@hotmail.com>
2015-06-08 18:35:05 -0400

    Disale CORS Header

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var port  	 = process.env.PORT || 8080; 				// set the port
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var cors = require('cors')
 
 // configuration ===============================================================
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
@@ -13,16 +14,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-
-app.use(allowCrossDomain);
+app.use(cors());
 
 // routes ======================================================================
 require('./app/routes.js')(app);
