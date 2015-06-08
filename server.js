f02f394f48d29f7faa1927e0ae7e7dd843ce05b6
Manoj Mandam <manoj.op@hotmail.com>
2015-06-08 18:29:48 -0400

    Disale CORS Header

--- server.js
+++ server.js
@@ -19,21 +19,10 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'example.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 };
 
-//...
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'cool beans' }));
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
-
+app.use(allowCrossDomain);
 
 // routes ======================================================================
 require('./app/routes.js')(app);
